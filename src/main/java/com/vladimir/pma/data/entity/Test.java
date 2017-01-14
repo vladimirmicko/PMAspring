77ea99bbe01aae7324888ec2be8f356b2530c51b
Vladimir <vladimirmicko@gmail.com>
2017-01-14 17:27:33 +0100

    Test dto maps all the slides

--- src/main/java/com/vladimir/pma/data/entity/Test.java
+++ src/main/java/com/vladimir/pma/data/entity/Test.java
@@ -47,7 +47,7 @@ public class Test implements java.io.Serializable {
 	@Column(name = "CREATION_DATE", nullable = true)
 	private Date creationDate;
 	
-	@JsonIgnore
+//	@JsonIgnore
 	@OneToMany(mappedBy = "test")
 	@LazyCollection(LazyCollectionOption.FALSE)
 	@Fetch(FetchMode.SUBSELECT)

52266467d2e843b10e088c4696ab6760f87a642f
Vladimir Randjelovic <72micko@gmail.com>
2018-05-22 21:04:55 +0200

    F: Result class created

---
+++ src/main/webapp/app/result.ts
@@ -0,0 +1,12 @@
+
+export class Result {
+  id: number;
+  testId: number;
+  testName: string;
+  testTaken: Date;
+  testStartTime: string;
+  supervisedValue: number;
+  evaluation: number;
+  userId: number;
+}
+

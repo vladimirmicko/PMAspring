40e017b39a08e769e1ea3e29e2a2440a980721cd
Vladimir Randjelovic <vladimirmicko@gmail.com>
2018-06-08 16:19:15 +0200

    B: AIService, AIController. DatabaseUtils.props file removed

---
+++ src/main/java/com/vladimir/pma/controllers/AIController.java
@@ -0,0 +1,67 @@
+package com.vladimir.pma.controllers;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vladimir.pma.common.utility.Utility;
+import com.vladimir.pma.data.dao.ResultDao;
+import com.vladimir.pma.data.dao.StatisticsDao;
+import com.vladimir.pma.data.dao.TestDao;
+import com.vladimir.pma.data.dto.Statistics;
+import com.vladimir.pma.data.entity.Answer;
+import com.vladimir.pma.data.entity.Result;
+import com.vladimir.pma.data.entity.Test;
+import com.vladimir.pma.data.entity.UserAccount;
+import com.vladimir.pma.features.ai.AIService;
+import com.vladimir.pma.features.statistics.StatisticsService;
+import com.vladimir.pma.security.SecurityUtils;
+
+@RestController
+@RequestMapping("/rest/ai")
+public class AIController {
+	private static final Log log = LogFactory.getLog(AIController.class);
+
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Autowired
+	private ResultDao resultDao;
+	
+	@Autowired
+	private TestDao testDao;
+
+	@Autowired
+	private StatisticsDao statisticsDao;
+	
+	@Autowired
+	private StatisticsService statisticsService;
+
+	@Autowired
+	private AIService aiService;
+	
+	
+	
+	@RequestMapping(value = "/train", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> train() {
+		log.info("train(): /rest/ai ");
+		aiService.getInstancesFromDB();
+		
+		return new ResponseEntity<String>("OK", HttpStatus.OK);
+	}
+
+
+}
--- src/main/java/com/vladimir/pma/features/ai/AIService.java
+++ src/main/java/com/vladimir/pma/features/ai/AIService.java
@@ -1,13 +1,21 @@
 package com.vladimir.pma.features.ai;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import com.vladimir.pma.features.statistics.StatisticsService;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.experiment.InstanceQuery;
 
@@ -15,59 +23,88 @@ import weka.experiment.InstanceQuery;
 public class AIService {
 
 	private static final Log log = LogFactory.getLog(AIService.class);
-	
-	public void getInstancesFromDB(){
 
-		try {
-			InstanceQuery query = new InstanceQuery();
+	public void getInstancesFromDB(){
 			
-			 query.setUsername("VLADIMIR");
-			 query.setPassword("VLADIMIR");
-			 query.setQuery("select * from results");
-			 
-			 // You can declare that your data set is sparse
-			 // query.setSparseData(true);
-			 
-			 Instances data = query.retrieveInstances();
-		//	
-//			// Declare two numeric attributes
-//			Attribute Attribute1 = new Attribute("firstNumeric");
-//			Attribute Attribute2 = new Attribute("secondNumeric");
-		//	
-//			// Declare a nominal attribute along with its values
-//			FastVector fvNominalVal = new FastVector(3);
-//			fvNominalVal.addElement("blue");
-//			fvNominalVal.addElement("gray");
-//			fvNominalVal.addElement("black");
-//			Attribute Attribute3 = new Attribute("aNominal", fvNominalVal);
-		//	
-//			// Declare the class attribute along with its values
-//			FastVector fvClassVal = new FastVector(2);
-//			fvClassVal.addElement("positive");
-//			fvClassVal.addElement("negative");
-//			Attribute ClassAttribute = new Attribute("theClass", fvClassVal);
-		//	
-//			// Declare the feature vector
-//			FastVector fvWekaAttributes = new FastVector(4);
-//			fvWekaAttributes.addElement(Attribute1);
-//			fvWekaAttributes.addElement(Attribute2);
-//			fvWekaAttributes.addElement(Attribute3);
-//			fvWekaAttributes.addElement(ClassAttribute);
+			// Declare two numeric attributes
+			Attribute Attribute1 = new Attribute("firstNumeric");
+			Attribute Attribute2 = new Attribute("secondNumeric");
+			
+			// Declare a nominal attribute along with its values
+			List fvNominalVal = new ArrayList();
+			fvNominalVal.add("blue");
+			fvNominalVal.add("gray");
+			fvNominalVal.add("black");
+			Attribute Attribute3 = new Attribute("aNominal", fvNominalVal);
+			
+			// Declare the class attribute along with its values
+			List fvClassVal = new ArrayList();
+			fvClassVal.add("positive");
+			fvClassVal.add("negative");
+			Attribute ClassAttribute = new Attribute("theClass", fvClassVal);
+			
+			// Declare the feature vector
+			List fvWekaAttributes = new ArrayList();
+			fvWekaAttributes.add(Attribute1);
+			fvWekaAttributes.add(Attribute2);
+			fvWekaAttributes.add(Attribute3);
+			fvWekaAttributes.add(ClassAttribute);
+			
+			// Create an empty training set
+			Instances isTrainingSet = new Instances("Rel", (ArrayList<Attribute>) fvWekaAttributes, 10);
+			// Set class index
+			isTrainingSet.setClassIndex(3);
+			
+			// Create the instance
+			Instance iExample = new DenseInstance(4);
+			iExample.setValue((Attribute)fvWekaAttributes.get(0), 1.0);
+			iExample.setValue((Attribute)fvWekaAttributes.get(1), 0.5);
+			iExample.setValue((Attribute)fvWekaAttributes.get(2), "gray");
+			iExample.setValue((Attribute)fvWekaAttributes.get(3), "positive");
+			
+			// add the instance
+			isTrainingSet.add(iExample);
+			
+			// Create a naïve bayes classifier
+			Classifier cModel = (Classifier)new NaiveBayes();
+			try {
+				cModel.buildClassifier(isTrainingSet);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
 			
 			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			// Create an empty training set
+			Instances isTestingSet = new Instances("Rel", (ArrayList<Attribute>) fvWekaAttributes, 10);
+			// Set class index
+			isTestingSet.setClassIndex(3);
+			
+			// Create the instance
+			Instance iExampleTest = new DenseInstance(4);
+			iExampleTest.setValue((Attribute)fvWekaAttributes.get(0), 1.0);
+			iExampleTest.setValue((Attribute)fvWekaAttributes.get(1), 0.5);
+			iExampleTest.setValue((Attribute)fvWekaAttributes.get(2), "gray");
+			iExampleTest.setValue((Attribute)fvWekaAttributes.get(3), "positive");
+			
+			
+			// Test the model
+			Evaluation eTest=null;
+			try {
+				eTest = new Evaluation(isTrainingSet);
+				eTest.evaluateModel(cModel, isTestingSet);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			// Print the result à la Weka explorer:
+			String strSummary = eTest.toSummaryString();
+			System.out.println(strSummary);
+			// Get the confusion matrix
+			double[][] cmMatrix = eTest.confusionMatrix();			
+			
 
 	}
-	
-	
 
-
-
-
-	
-	
-	
 }
